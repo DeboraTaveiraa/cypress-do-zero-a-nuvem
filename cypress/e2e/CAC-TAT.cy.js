f922274574ffbb7873bba774b47cdba47171876a
DeboraTaveiraa <deborataveirapvh@gmail.com>
2026-04-04 20:10:40 -0300

    fix: update button selector to use type attribute and refactor fillMandatoryFieldsAndSubmit command

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -18,22 +18,20 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('#email').should('have.value', 'deborataveira.dev@gmail.com')
     cy.get('#open-text-area').should('have.value', 'Testando...')
 
-    cy.contains('button', 'Enviar').click()
+    cy.contains('button[type="submit"]', 'Enviar').click()
 
     cy.get('.success').should('be.visible')
   })
 
   it('preenche os campos obrigatórios e envia o formulário - textarea com delay 0', () => {
+    const longText = Cypress._.repeat('Veniam aliquip commodo commodo eiusmod deserunt non tempor nulla.', 10)
+
     cy.get('#firstName').type('Débora')
     cy.get('#lastName').type('Taveira')
     cy.get('#email').type('deborataveira.dev@gmail.com')
-    cy.get('#open-text-area')
-      .type(
-        'Veniam aliquip commodo commodo eiusmod deserunt non tempor nulla. Laboris ea anim exercitation aliquip et fugiat amet proident ea nulla elit ad incididunt Lorem. Proident cillum magna minim voluptate exercitation sunt nulla. Anim officia pariatur tempor pariatur. Est consequat culpa ullamco est eu occaecat aliqua exercitation mollit sint aute irure. Consectetur enim cillum veniam occaecat enim quis aliqua laboris fugiat ipsum. Sit aute sunt exercitation sunt ipsum consequat anim aliquip ullamco consequat.Et excepteur sit aute tempor ullamco.Eiusmod ea qui id nulla eiusmod ea occaecat in.Tempor dolor esse pariatur nostrud aliquip. Tempor laborum quis incididunt elit cillum do ea magna.Deserunt fugiat eu non officia laboris cupidatat dolor excepteur qui excepteur sit anim laboris ut.Ut cillum exercitation magna aliquip amet non commodo labore nulla ex proident enim sunt consequat.', 
-        { delay: 0 }
-      )
+    cy.get('#open-text-area').type(longText, { delay: 0 })
     
-    cy.contains('button', 'Enviar').click()
+    cy.contains('button[type="submit"]', 'Enviar').click()
 
     cy.get('.success').should('be.visible')
   })
@@ -44,7 +42,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('#email').type('deborataveira.dev')
     cy.get('#open-text-area').type('Testando...')
 
-    cy.contains('button', 'Enviar').click()
+    cy.contains('button[type="submit"]', 'Enviar').click()
 
     cy.get('.error').should('be.visible')
   })
@@ -61,7 +59,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('#open-text-area').type('Testando...')
 
     cy.get('#phone-checkbox').click()
-    cy.contains('button', 'Enviar').click()
+    cy.contains('button[type="submit"]', 'Enviar').click()
 
     cy.get('.error').should('be.visible')
   })
@@ -89,24 +87,24 @@ describe('Central de Atendimento ao Cliente TAT', () => {
   })
 
   it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
-    cy.contains('button', 'Enviar').click()
+    cy.contains('button[type="submit"]', 'Enviar').click()
 
     cy.get('.error > strong').should('be.visible')
   })
 
-  it('envia o formuário com sucesso usando um comando customizado', () => {
+  it('envia o formuário com sucesso usando um comando customizado com usuário default', () => {
     cy.fillMandatoryFieldsAndSubmit()
+
+    cy.get('.success').should('be.visible')
   })
 
-  it('envia o formuário com sucesso usando um comando customizado que recebe argumento', () => {
-    cy.fillMandatoryFieldsAndSubmitWithArgument({
+  it('envia o formuário com sucesso usando um comando customizado passando dados do usuário', () => {
+    cy.fillMandatoryFieldsAndSubmit({
       name: 'Débora',
       lastName: 'Taveira',
       email: 'deborataveira.dev@gmail.com'
     })
-  })
 
-  it('envia o formuário com sucesso usando um comando customizado que tem argumento default', () => {
-    cy.fillMandatoryFieldsAndSubmitWithArgumentDefault()
+    cy.get('.success').should('be.visible')
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,68 +1,8 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-
-Cypress.Commands.add('fillMandatoryFieldsAndSubmit', () => {
-  cy.get('#firstName').type('Débora')
-  cy.get('#lastName').type('Taveira')
-  cy.get('#email').type('deborataveira.dev@gmail.com')
-  cy.get('#open-text-area').type('Testando...')
-
-  cy.get('#firstName').should('have.value', 'Débora')
-  cy.get('#lastName').should('have.value', 'Taveira')
-  cy.get('#email').should('have.value', 'deborataveira.dev@gmail.com')
-  cy.get('#open-text-area').should('have.value', 'Testando...')
-
-  cy.contains('button', 'Enviar').click()
-
-  cy.get('.success').should('be.visible')
-})
-
-Cypress.Commands.add('fillMandatoryFieldsAndSubmitWithArgument', (user) => {
-  cy.get('#firstName').type(user.name)
-  cy.get('#lastName').type(user.lastName)
-  cy.get('#email').type(user.email)
-  cy.get('#open-text-area').type('Testando...')
-
-  cy.get('#firstName').should('have.value', user.name)
-  cy.get('#lastName').should('have.value', user.lastName)
-  cy.get('#email').should('have.value', user.email)
-  cy.get('#open-text-area').should('have.value', 'Testando...')
-
-  cy.contains('button', 'Enviar').click()
-
-  cy.get('.success').should('be.visible')
-})
-
-const USER_DEFAULT = {
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', (user = {
   name: 'Débora',
   lastName: 'Taveira',
   email: 'deborataveira.dev@gmail.com'
-}
-
-Cypress.Commands.add('fillMandatoryFieldsAndSubmitWithArgumentDefault', (user = USER_DEFAULT) => {
+}) => {
   cy.get('#firstName').type(user.name)
   cy.get('#lastName').type(user.lastName)
   cy.get('#email').type(user.email)
@@ -73,8 +13,6 @@ Cypress.Commands.add('fillMandatoryFieldsAndSubmitWithArgumentDefault', (user = 
   cy.get('#email').should('have.value', user.email)
   cy.get('#open-text-area').should('have.value', 'Testando...')
 
-  cy.contains('button', 'Enviar').click()
-
-  cy.get('.success').should('be.visible')
+  cy.contains('button[type="submit"]', 'Enviar').click()
 })
 
