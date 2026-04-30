c5ecc167295afc3bdb390c63a91cf94775f2ee6b
DeboraTaveiraa <deborataveirapvh@gmail.com>
2026-04-30 18:01:40 -0300

    feat: add test for filling text area using invoke command

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -256,4 +256,10 @@ describe('Central de Atendimento ao Cliente TAT', () => {
       .invoke('hide')
       .should('not.be.visible')
   })
+
+  it('preenche o campo da área de texto usando o comando invoke', () => {
+    cy.get('#open-text-area')
+      .invoke('val', 'um texto excrito pelo invoke')
+      .should('have.value', 'um texto excrito pelo invoke')
+  })
 })
